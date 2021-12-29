3ce3239985067a5f5ca9f0bcc0c624c65e15e5e3
mandeeps207 <51690189+mandeeps207@users.noreply.github.com>
2021-12-29 20:42:13 +0530

    Update home.php

--- home.php
+++ home.php
@@ -24,7 +24,7 @@ else {
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Filepond User Picture</title>
-    <link rel="stylesheet" href="./bootstrap/bootstrap.min.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
 </head>
 <body>
     <div class="container mt-3">
