b471f07377a5ed47d7515ed1f7f0876d26318ddd
mandeeps207 <51690189+mandeeps207@users.noreply.github.com>
2021-12-29 20:43:10 +0530

    Update logout.php

--- logout.php
+++ logout.php
@@ -9,7 +9,7 @@ session_start();
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Logout</title>
-    <link rel="stylesheet" href="./bootstrap/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css">
 </head>
 <body>
     <div class='container mt-3'>
